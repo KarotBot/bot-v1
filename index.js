1ef34948ff1855e979b5d385be8c303ba4c7d8a9
Slenky <slenky@protonmail.com>
2021-03-19 17:41:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const fs              = require('fs');
+const Discord         = require('discord.js');
+const {prefix, token} = require('./config.json');
+const client          = new Discord.Client();
+client.commands       = new Discord.Collection();
+const commandFiles    = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const cooldowns       = new Discord.Collection();
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.on("ready", () => {
+  console.log("Zapínam sa... (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ ✧ﾟ･: *ヽ(◕ヮ◕ヽ)")
+  client.user.setActivity(`.k help`, { type: 2});
+  console.log("Status úspešne načítaný! (▀̿Ĺ̯▀̿ ̿)")
+});
+
+client.on('message', async(message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+	const command = client.commands.get(commandName)
+		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+	if (!command) return;
+
+	if (command.guildOnly && message.channel.type === 'dm') {
+		return;
+	}
+
+	if (!cooldowns.has(command.name)) {
+		cooldowns.set(command.name, new Discord.Collection());
+	}
+
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+
+	if (timestamps.has(message.author.id)) {
+		const time = timestamps.get(message.author.id) + cooldownAmount;
+
+		if (now < time) {
+			const timeLeft = (time - now) / 1000;
+			return message.reply(`Počkaj ${timeLeft.toFixed(1)} pred použitím príkazu! \`${command.name}\`.`);
+		}
+	}
+
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	}catch{
+		console.log('Error ¯\_(ツ)_/¯');
+	}
+});
+
+client.login(token);

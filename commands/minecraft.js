d07ccf198e21d52dcf6324ee04ebc1196b8615f1
SebestikCZ <44745014+SebestikCZ@users.noreply.github.com>
2021-04-13 15:52:56 +0200

    Update minecraft.js

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -6,7 +6,7 @@ module.exports = {
 	description: 'Posle meme',
   aliases: ['mc'],
 	cooldown: 4,
-    	async execute(message, args) {
+    	async execute(client, message, args) {
         var buff = (await axios({
             url: new URL("https://api.hyrousek.tk/useless/reddit?reddit=minecraft").toString(),
             headers: {"Authorization":"zmizni"},

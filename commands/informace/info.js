01363bb34b520d02d1a09201f2a93730f7fb32a5
Šebestíček <44745014+SebestikCZ@users.noreply.github.com>
2021-04-17 09:25:25 +0200

    Malinká úprava infa

--- commands/informace/info.js
+++ commands/informace/info.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js")
+const { prefix } = require("../../config.json");
+const db = require("quick.db");
 const os = require("os")
 
 module.exports = {
@@ -7,12 +9,18 @@ module.exports = {
     description: 'Pošle moje infošky',
     category: "informace",
     async execute(client, message, args) {
+        var in_prefix = prefix;
+        if (message.guild) {
+            if (db.has(message.guild.id)) {
+                in_prefix = db.get(message.guild.id);
+			}
+		}
     const embed = new Discord.MessageEmbed()
     .setColor('#e54918')
     .setTitle('Karot')
     .setDescription('Karot je slovenský Discord bot zameraný na rôzne moderačné, zábavné a informačné príkazy.')
     .addField('Web:', '[www.karot.xyz](https://karot.xyz)')
-    .addField('Prefix:', '`+`')
+    .addField('Prefix:', `\`${in_prefix}\``)
     .addField(`Ping`, `${Date.now() - message.createdTimestamp}ms`)
     .addField(`Využitie RAM:`, `${Math.trunc((process.memoryUsage().heapUsed) / 1024 / 1000)} MB / ${Math.trunc(os.totalmem() / 1024 / 1000)} MB (${Math.round((Math.round(process.memoryUsage().heapUsed / 1024 / 1024) / Math.round(os.totalmem() / 1024 / 1024)) * 100)}%)`, true)
     .addField('Oficiálny Discord server:',  '[9Byp7mWfMF](https://discord.com/invite/9Byp7mWfMF)')

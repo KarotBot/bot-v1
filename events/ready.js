0de96666c87f732c6eed3fe5e5e4c1a1c2c02826
Slenky <slenky@protonmail.com>
2021-04-19 12:07:31 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,17 +2,17 @@ module.exports = {
     name: "ready",
     run: (client) => {
         const activities_list = [
-            `${client.guilds.cache.size} guild | +help`,
-            "www.karot.xyz | +help",
-            "#KarotGang | +help",
-        	"cc panda | +help",
-        	"Kekega | +help",
-        	"https://youtu.be/dQw4w9WgXcQ | +help",
-        	"som zemiak | +help",
-        	"help me im stuck in a discord bot and i wanna go home | +help",
-        	"im big chungusfortnite420 | +help",
-        	"a dostanem nytro???!!1!1!????? | +help",
-        	"i want to chug jug with you | +help",
+            `${client.guilds.cache.size} guild | !help`,
+            "www.karot.xyz | !help",
+            "#KarotGang | !help",
+        	"cc panda | !help",
+        	"Kekega | !help",
+        	"https://youtu.be/dQw4w9WgXcQ | !help",
+        	"som zemiak | !help",
+        	"help me im stuck in a discord bot and i wanna go home | !help",
+        	"im big chungusfortnite420 | !help",
+        	"a dostanem nytro???!!1!1!????? | !help",
+        	"i want to chug jug with you | !help",
             ];
         setInterval(() => {
             const index = Math.floor(Math.random() * (activities_list.length - 1) + 1);
